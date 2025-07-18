6dc6f1570e3850548db70507edf20451db39919b
sohan sindhav <sohann7704@gmail.com>
2025-07-18 23:40:52 +0530

    fix : backend cookie

--- Backend/controller/authController.js
+++ Backend/controller/authController.js
@@ -29,7 +29,7 @@ const login = async (req, res) => {
   res.cookie("token", token, {
     httpOnly: true,
     secure: false,
-    sameSite: "strict",
+    sameSite: "None",
     maxAge: 24 * 60 * 60 * 1000,
   });
   res.json({ message: "Login successful" });
